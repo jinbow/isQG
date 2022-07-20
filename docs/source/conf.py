30a8a623eb6ffdd0ef49a78dff6e4ebea4976d11
Jinbo Wang <jinbow@gmail.com>
2022-07-19 23:50:16 -0700

    document

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,7 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 #/ ones.
 
-import sphinx_rtd_theme
-
-extensions = [
-    'sphinx_rtd_theme',
+extensions = ['sphinx_rtd_theme',
 ]
 
 html_theme = "sphinx_rtd_theme"
